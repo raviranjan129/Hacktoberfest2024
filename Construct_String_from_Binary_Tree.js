660a8d3d6adbbee1f9ee819f7c4cd3139b67b211
Ravi Ranjan <rk6318633@gmail.com>
2024-10-06 13:02:13 +0530

    Create Construct_String_from_Binary_Tree.js

---
+++ Construct_String_from_Binary_Tree.js
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {string}
+ */
+
+  function f(root){
+    if(root == null ){
+        return "";
+    }
+    let left = f(root.left);
+    let right = f(root.right);
+
+    if(left == "" && right == ""){
+        return "" + root.val;
+    }else if(left == ""){
+        return root.val + "()" + "(" + right + ")";
+    }else if(right == ""){
+        return root.val + "(" + left + ")";
+    }else{
+        return root.val + "(" + left + ")" + "(" + right + ")";
+    }
+  }
+var tree2str = function(root) {
+    return f(root);
+};
